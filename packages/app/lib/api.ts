f06e6459899676bc4969eee8f2cd23bce716dec7
César Guadarrama C. <cesargdm@hey.com>
2024-09-06 16:12:09 -0600

    fix: add platform variable

--- packages/app/lib/api.ts
+++ packages/app/lib/api.ts
@@ -1,4 +1,7 @@
-export const API_BASE_URL = '/api/v1' // 'https://bichos-id.fucesa.com/api/v1'
+import { Platform } from 'react-native'
+
+export const API_BASE_URL =
+  Platform.OS === 'web' ? '/api/v1' : 'https://bichos-id.fucesa.com/api/v1'
 
 export const ASSETS_BASE_URL = 'https://bichos-id.assets.fucesa.com'
 
