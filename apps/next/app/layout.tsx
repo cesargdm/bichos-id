824066fd5b700cb367e652cdd9c7ef2767862b4e
César Guadarrama C. <cesargdm@hey.com>
2024-09-06 17:52:01 -0600

    fix: add favicon

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -16,7 +16,25 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.svg"/>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="icon" href="/favicon.svg" />
+      <link rel="manifest" href="/site.webmanifest" />
       <body>
         <StylesProvider>{children}</StylesProvider>
       </body>
