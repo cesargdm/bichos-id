2280e6e658c354f91b88be17423ad9b8b65c9a0d
César Guadarrama C. <cesargdm@hey.com>
2024-09-05 22:08:35 -0600

    fix: add missing dep

--- apps/next/app/styles-provider.tsx
+++ apps/next/app/styles-provider.tsx
@@ -1,11 +1,13 @@
 // @ts-nocheck
 'use client'
+
 import { useServerInsertedHTML } from 'next/navigation'
 import { StyleSheet } from 'react-native'
 
 export function StylesProvider({ children }: { children: React.ReactNode }) {
   useServerInsertedHTML(() => {
     const sheet = StyleSheet.getSheet()
+
     return (
       <style
         dangerouslySetInnerHTML={{ __html: sheet.textContent }}
@@ -13,5 +15,6 @@ export function StylesProvider({ children }: { children: React.ReactNode }) {
       />
     )
   })
-  return <>{children}</>
+
+  return children
 }
