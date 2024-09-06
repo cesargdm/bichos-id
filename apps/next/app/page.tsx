167350240829643903a8ad5f6bb7aa261f8a2d5f
César Guadarrama C. <cesargdm@hey.com>
2024-09-05 23:41:51 -0600

    fix: optimize metadata

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -3,12 +3,6 @@ import { Suspense } from 'react'
 
 import HomeScreen from '@bichos-id/app/screens/home'
 
-export const metadata: Metadata = {
-  title: 'Bichos ID - Fucesa',
-  description:
-    'Identifica insectos, arácnidos y otros bichos con Bichos ID usando inteligencia artificial.',
-}
-
 export default function HomePage() {
   return (
     <Suspense fallback={null}>
