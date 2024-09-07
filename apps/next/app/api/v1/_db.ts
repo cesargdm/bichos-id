470e369c119f55d13e2aed5f3dd9c014462f9bbf
César Guadarrama C. <cesargdm@hey.com>
2024-09-07 00:07:30 -0600

    feat: improve prompt

--- apps/next/app/api/v1/_db.ts
+++ apps/next/app/api/v1/_db.ts
@@ -1,26 +1,28 @@
 import { z } from 'zod'
 
-export const OrganismSchema = z.object({
-  identification: z.object({
-    commonName: z.string(),
-    scientificClassification: z.object({
-      family: z.string(),
-      genus: z.string(),
-      species: z.string().optional(),
+export const OrganismSchema = z
+  .object({
+    identification: z.object({
+      commonName: z.string(),
+      scientificClassification: z.object({
+        family: z.string(),
+        genus: z.string(),
+        species: z.string().optional(),
+      }),
+      description: z.string().optional(),
+      venomous: z.object({
+        type: z.string().optional(),
+        level: z.union([
+          z.literal('NON_VENOMOUS'),
+          z.literal('MILDLY_VENOMOUS'),
+          z.literal('VENOMOUS'),
+          z.literal('HIGHLY_VENOMOUS'),
+        ]),
+      }),
     }),
-    description: z.string().optional(),
-    venomous: z.object({
-      type: z.string().optional(),
-      level: z.union([
-        z.literal('NON_VENOMOUS'),
-        z.literal('MILDLY_VENOMOUS'),
-        z.literal('VENOMOUS'),
-        z.literal('HIGHLY_VENOMOUS'),
-      ]),
-    }),
-  }),
-  confidence: z.number(),
-})
+    confidence: z.number(),
+  })
+  .optional()
 
 export interface Organism {
   id: string
--- apps/next/app/api/v1/ai/vision/route.ts
+++ apps/next/app/api/v1/ai/vision/route.ts
@@ -49,6 +49,7 @@ export async function POST(request: NextRequest) {
           role: 'system',
           content: `You are an expert entomologist that will recognize species based on a photo.
 Use shape, color and subject surroundings and metadata to archive the best identification.
+Do not return any information if the photo is inappropriate, blurry or unrelated.
 Try to be as accurate as possible with family, genus and species, if unsure, return 'sp' as species.`,
         },
         {
@@ -70,7 +71,10 @@ Try to be as accurate as possible with family, genus and species, if unsure, ret
       response_format: zodResponseFormat(OrganismSchema, 'event'),
     })
 
-    if (!response?.choices?.[0]?.message?.parsed) {
+    if (
+      !response?.choices?.[0]?.message?.parsed ||
+      !response.choices[0].message.parsed.identification
+    ) {
       return NextResponse.json(
         { error: 'No response from AI' },
         { status: 500 },
