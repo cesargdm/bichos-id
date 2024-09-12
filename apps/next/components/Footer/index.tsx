5ddfe6d7f5441009d813e320aa11c78d7daf234c
César Guadarrama C. <cesargdm@hey.com>
2024-09-11 22:40:27 -0600

    feat: add placeholder stores badge

--- apps/next/components/Footer/index.tsx
+++ apps/next/components/Footer/index.tsx
@@ -1,17 +1,34 @@
 import './styles.css'
 
+import appStore from './app-store.svg'
+import googlePlay from './google-play.svg'
+import Image from 'next/image'
+
 export default function Footer() {
   return (
     <footer>
       <div className="content">
         <p>
-          Esta herramienta se ofrece tal cual, sin ninguna garantía. No nos
-          hacemos responsables de cualquier daño causado por su uso.
+          Esta herramienta por ser preeliminar se ofrece tal cual, sin ninguna
+          garantía. No nos hacemos responsables de cualquier daño causado por su
+          uso.
         </p>
         <p>
           © {new Date().getFullYear()} Bichos ID de{' '}
           <a href="https://fucesa.com">Fucesa</a>
         </p>
+        <ul>
+          <li>
+            <a href="https://apps.apple.com">
+              <Image src={appStore} alt="App Store" />
+            </a>
+          </li>
+          <li>
+            <a href="https://play.google.com">
+              <Image src={googlePlay} alt="Google Play" />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   )
