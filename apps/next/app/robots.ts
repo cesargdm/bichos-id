39afd41e61f616aba2e03f32dc5971e265d462a7
César Guadarrama C. <cesargdm@hey.com>
2024-09-18 21:55:39 -0600

    fix: update robots config

--- apps/next/app/robots.ts
+++ apps/next/app/robots.ts
@@ -1,11 +1,13 @@
 import type { MetadataRoute } from 'next'
 
+const origin = process.env.NEXT_PUBLIC_ORIGIN
+
 export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			allow: '/',
 			userAgent: '*',
 		},
-		sitemap: '/sitemap.xml',
+		sitemap: `${origin}/sitemap.xml`,
 	}
 }
